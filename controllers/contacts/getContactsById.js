1785bfe4fdb4f812db7e9c41766f55feb2675874
Denis <denis.tolmachev@gmail.com>
2022-10-03 20:40:18 +0300

    fix: middlewares and controllers, helpers and validation established

---
+++ controllers/contacts/getContactsById.js
@@ -0,0 +1,13 @@
+const contacts = require("../../models/contacts");
+const { RequestError } = require("../../helpers");
+
+const getContactsById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw RequestError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = getContactsById;
