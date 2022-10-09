a6f021190172175faa9e02f5eb3d9da07a03cc1b
Denis <denis.tolmachev@gmail.com>
2022-10-09 20:45:27 +0300

    remove Joi schema & added handleSaveErrors middleware

--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,7 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleSaveErrors } = require("../helpers");
 
 const contactSchema = new Schema(
   {
@@ -21,6 +24,19 @@ const contactSchema = new Schema(
   { versionKey: false }
 );
 
+contactSchema.post("save", handleSaveErrors);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const schemas = {
+  addSchema,
+};
+
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+module.exports = { Contact, schemas };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/contacts");
 
 const { validateBody } = require("../../middlewares");
 
-const schemas = require("../../schemas/contactSchema");
+const { schemas } = require("../../models/contact");
 
 const { ctrlWrapper } = require("../../helpers");
 
