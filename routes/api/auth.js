5755a336c1c6f9c8a21117e6411a671988a29ca1
Denis <denis.tolmachev@gmail.com>
2022-10-11 21:30:15 +0300

    added auth api & user model

---
+++ routes/api/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// const { validateBody } = require("../../middlewares");
+
+// const { ctrlWrapper } = require("../../helpers");
+
+const router = express.Router();
+
+
+
+router.post("/register");
+
+
+module.exports = router;
