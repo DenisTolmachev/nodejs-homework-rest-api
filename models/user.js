9bba2ca082debc2d5d886bd8868f4b54cf65c77b
Denis <denis.tolmachev@gmail.com>
2022-10-13 20:49:25 +0300

    add signup controller & fix errors

--- models/user.js
+++ models/user.js
@@ -32,19 +32,19 @@ const userSchema = new Schema(
 
 userSchema.post("save", handleSaveErrors);
 
-const registerSchema = Joi.object({
+const signupSchema = Joi.object({
   // name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().pattern().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
 
 const schemas = {
-  registerSchema,
+  signupSchema,
   loginSchema,
 };
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,19 @@
 const express = require("express");
 
-// const { validateBody } = require("../../middlewares");
+const { validateBody } = require("../../middlewares");
 
-// const { ctrlWrapper } = require("../../helpers");
+const { ctrlWrapper } = require("../../helpers");
+
+const { schemas } = require("../../models/user");
+
+const ctrl = require("../../controllers/users");
 
 const router = express.Router();
 
-
-
-// router.post("/signup", validateBody());
-
+router.post(
+  "/signup",
+  validateBody(schemas.signupSchema),
+  ctrlWrapper(ctrl.signup)
+);
 
 module.exports = router;
