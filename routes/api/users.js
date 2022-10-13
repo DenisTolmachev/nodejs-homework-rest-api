f462fd48f95ee8ccd649050e73d826bf6322ab3e
Denis <denis.tolmachev@gmail.com>
2022-10-13 20:13:31 +0300

    correct api & usersRouter

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// const { validateBody } = require("../../middlewares");
+
+// const { ctrlWrapper } = require("../../helpers");
+
+const router = express.Router();
+
+
+
+// router.post("/signup", validateBody());
+
+
+module.exports = router;
