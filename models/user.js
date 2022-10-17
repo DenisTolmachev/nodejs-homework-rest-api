33d070802ffa3901bfba576f304a95fab5bfb148
Denis <denis.tolmachev@gmail.com>
2022-10-17 21:50:28 +0300

    fix: correction of comments

--- models/user.js
+++ models/user.js
@@ -6,46 +6,46 @@ const { handleSaveErrors } = require("../helpers");
 const emailRegexp = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
 
 const userSchema = new Schema(
-  {
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      match: emailRegexp,
-      unique: true,
-    },
-    subscription: {
-      type: String,
-      enum: ["starter", "pro", "business"],
-      default: "starter",
-    },
-    token: {
-      type: String,
-      default: null,
-    },
-  },
-  { versionKey: false, timestamps: true }
+	{
+		password: {
+			type: String,
+			required: [true, "Password is required"],
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			match: emailRegexp,
+			unique: true,
+		},
+		subscription: {
+			type: String,
+			enum: ["starter", "pro", "business"],
+			default: "starter",
+		},
+		token: {
+			type: String,
+			default: null,
+		},
+	},
+	{ versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", handleSaveErrors);
 
 const signupSchema = Joi.object({
-  // name: Joi.string().required(),
-  email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
+	email: Joi.string().pattern(emailRegexp).required(),
+	password: Joi.string().min(6).required(),
+	subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
+	email: Joi.string().pattern(emailRegexp).required(),
+	password: Joi.string().min(6).required(),
 });
 
 const schemas = {
-  signupSchema,
-  loginSchema,
+	signupSchema,
+	loginSchema,
 };
 
 const User = model("user", userSchema);
